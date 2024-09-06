4deb09ad5b69be00b708e2af83323e7f8eeeb2ba
Damien Boisvert <damien@alphagame.dev>
2024-09-06 12:50:56 -0700

    Automatically install plugins if nonexistant

--- zsh/rc.sh
+++ zsh/rc.sh
@@ -1,16 +1,21 @@
 # zshrc
 # to be sourced in.  Symlink to ~/.zshrc
 
-# If you come from bash you might have to change your $PATH.
-# export PATH=$HOME/bin:$HOME/.local/bin:/usr/local/bin:$PATH
-
-# Path to your Oh My Zsh installation.
-
 # ----- install stuff if not already installed -----
+p="zshrc:"
 PLUGINSDIR="$HOME/.oh-my-zsh/custom/plugins"
-if [ ! -d "$PLUGINSDIR/command-time" ]; then
-  git clone https://github.com/popstas/zsh-command-time.git ~/.oh-my-zsh/custom/plugins/command-time
-fi
+function pull_plugin_from_git {
+	# usage: <id> <repo>
+	if [ ! -d "$PLUGINSDIR/$1" ]; then
+		echo "$p $1 doesn't exist (Installing it!)"
+		# SILENCE IT. IT MUST NOT SPEAK
+		git clone $2 $PLUGINSDIR/$1 2> /dev/null
+	fi
+}
+
+pull_plugin_from_git command-time https://github.com/popstas/zsh-command-time.git
+pull_plugin_from_git zsh-syntax-highlighting https://github.com/zsh-users/zsh-syntax-highlighting.git
+# --------------------------------------------------
 export ZSH="$HOME/.oh-my-zsh"
 
 ZSH_THEME="agnoster"
@@ -19,8 +24,16 @@ ZSH_COMMAND_TIME_MIN_SECONDS=3
 ZSH_COMMAND_TIME_MSG="Execution time: %s sec"
 ZSH_COMMAND_TIME_COLOR="cyan"
 
-plugins=(git command-time copyfile)
+plugins=(git command-time copyfile zsh-syntax-highlighting)
 
 source $ZSH/oh-my-zsh.sh
+
+# Change if needed
 source $HOME/.dotfiles/import_all.sh
 
+# I did not write this.  My friend did.  Please try to decipher it.
+# =================================================================
+# Long ago the master of spinjitsu created the sexy man boggy-poo, 
+# master of all elements. But then the evil creature, DMEFRENCHOY 
+# betayed boggy-poo and began a century long war to obtain the egg, 
+# which containe the re encarnation of JeSUS Chist.
