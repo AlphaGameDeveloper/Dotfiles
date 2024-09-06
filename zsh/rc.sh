9b55c9b8c644673393e1e4504b1940dd75c5cd8f
Damien Boisvert <damien@alphagame.dev>
2024-09-05 17:28:16 -0700

    Add automatic downloads for plugins if they're not installed

--- zsh/rc.sh
+++ zsh/rc.sh
@@ -5,6 +5,12 @@
 # export PATH=$HOME/bin:$HOME/.local/bin:/usr/local/bin:$PATH
 
 # Path to your Oh My Zsh installation.
+
+# ----- install stuff if not already installed -----
+PLUGINSDIR="$HOME/.oh-my-zsh/custom/plugins"
+if [ ! -d "$PLUGINSDIR/command-time" ]; then
+  git clone https://github.com/popstas/zsh-command-time.git ~/.oh-my-zsh/custom/plugins/command-time
+fi
 export ZSH="$HOME/.oh-my-zsh"
 
 ZSH_THEME="agnoster"
