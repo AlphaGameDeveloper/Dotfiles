3f9fcde5fbb22fd41197847db8641a146e881687
Damien Boisvert <damien@alphagame.dev>
2024-09-10 08:56:04 -0700

    Use PowerLevel10k theme

--- zsh/rc.sh
+++ zsh/rc.sh
@@ -1,27 +1,47 @@
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
 # zshrc
 # to be sourced in.  Symlink to ~/.zshrc
 
 # ----- install stuff if not already installed -----
 p="zshrc:"
 PLUGINSDIR="$HOME/.oh-my-zsh/custom/plugins"
+THEMESDIR="$HOME/.oh-my-zsh/custom/themes"
 function pull_plugin_from_git {
 	# usage: <id> <repo>
 	if [ ! -d "$PLUGINSDIR/$1" ]; then
-		echo "$p $1 doesn't exist (Installing it!)"
+		echo "$p Plugin '$1' doesn't exist (Installing it!)"
 		# SILENCE IT. IT MUST NOT SPEAK
-		git clone $2 $PLUGINSDIR/$1 2> /dev/null
+		git clone $2 $PLUGINSDIR/$1 --depth 1 2> /dev/null
 	fi
 }
+
+function pull_theme_from_git {
+	# usage: <id> <repo>
+	if [ ! -d "$THEMESDIR/$1" ]; then
+		echo "$p Theme '$1' doesn't exist (Installing it!)"
+		# SILENCE IT. IT MUST NOT SPEAK
+		git clone $2 $THEMESDIR/$1 --depth 1 2> /dev/null
+	fi
+}
+
+
 
 pull_plugin_from_git command-time https://github.com/popstas/zsh-command-time.git
 pull_plugin_from_git zsh-syntax-highlighting https://github.com/zsh-users/zsh-syntax-highlighting.git
 pull_plugin_from_git you-should-use https://github.com/MichaelAquilina/zsh-you-should-use.git
-pull_plugin_from_git zsh-autosuggestions https://github.com/zsh-users/zsh-autosuggestions
+pull_plugin_from_git zsh-autosuggestions https://github.com/zsh-users/zsh-autosuggestions.git
 
+pull_theme_from_git  powerlevel10k https://github.com/romkatv/powerlevel10k.git
 # --------------------------------------------------
 export ZSH="$HOME/.oh-my-zsh"
 
-ZSH_THEME="agnoster"
+ZSH_THEME="powerlevel10k/powerlevel10k"
 
 ZSH_COMMAND_TIME_MIN_SECONDS=3
 ZSH_COMMAND_TIME_MSG="Execution time: %s sec"
@@ -48,3 +68,6 @@ source $HOME/.dotfiles/import_all.sh
 # master of all elements. But then the evil creature, DMEFRENCHOY 
 # betayed boggy-poo and began a century long war to obtain the egg, 
 # which containe the re encarnation of JeSUS Chist.
+
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
