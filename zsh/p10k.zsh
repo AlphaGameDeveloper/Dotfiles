327c6f304d1ba855ee3e594d381c845e4176a02a
Damien Boisvert <damien@alphagame.dev>
2025-01-12 22:03:53 -0800

    Add some information in the prompt!

--- zsh/p10k.zsh
+++ zsh/p10k.zsh
@@ -98,24 +98,28 @@
     chezmoi_shell           # chezmoi shell (https://www.chezmoi.io/)
     # vi_mode               # vi mode (you don't need this if you've enabled prompt_char)
     # vpn_ip                # virtual private network indicator
-     load                  # CPU load
+    # load                  # CPU load
     # disk_usage            # disk usage
-     ram                   # free RAM
+    # ram                   # free RAM
     # swap                  # used swap
     todo                    # todo items (https://github.com/todotxt/todo.txt-cli)
     timewarrior             # timewarrior tracking status (https://timewarrior.net/)
     taskwarrior             # taskwarrior task count (https://taskwarrior.org/)
     per_directory_history   # Oh My Zsh per-directory-history local/global indicator
     # cpu_arch              # CPU architecture
-    time                    # current time
+    # time                    # current time
     # =========================[ Line #2 ]=========================
-    newline                 # \n
+    # newline                 # \n
      ip                    # ip address and bandwidth usage for a specified network interface
-     public_ip             # public IP address
+    # public_ip             # public IP address
     # proxy                 # system-wide http/https/ftp proxy
-     battery               # internal battery
+                    # internal battery
      wifi                  # wifi speed
     # example               # example user-defined segment (see prompt_example function below)
+    newline
+    
+    battery
+    time
   )
 
   # Defines character set used by powerlevel10k. It's best to let `p10k configure` set it for you.
